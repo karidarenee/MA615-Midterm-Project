4588a26af3aba319493080c632bcbb5b86f16944
Rose Determan <rosed@bu.edu>
2021-10-25 16:44:51 -0400

    begin isolating chemical names

--- wrangling.R
+++ wrangling.R
@@ -3,3 +3,7 @@
 # GOAL: have one dataframe with the following columns: 
 # chemical, chemical type (insecticide, herbicide, fungicide, fertilizer), 
 # toxicity-human/bee, toxicity-level, state, year, and measurement(s) 
+
+
+pesticide <- read.csv("Pesticides.csv")
+straw <- read.csv("Strawberries.csv")
