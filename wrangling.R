4257499b5050c642d07edc170f0c653a85c47791
Rose Determan <rosed@bu.edu>
2021-10-25 15:14:57 -0400

    add wrangling.R

---
+++ wrangling.R
@@ -0,0 +1,5 @@
+# Wrangling file
+
+# GOAL: have one dataframe with the following columns: 
+# chemical, chemical type (insecticide, herbicide, fungicide, fertilizer), 
+# toxicity-human/bee, toxicity-level, state, year, and measurement(s) 
