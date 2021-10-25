f9fe255f17d7109b1bccc063ee56892c7324160e
smartvickyshan2000 <smartvickyshan@gmail.com>
2021-10-25 15:21:47 -0400

    read two csv files

--- wrangling.R
+++ wrangling.R
@@ -3,3 +3,6 @@
 # GOAL: have one dataframe with the following columns: 
 # chemical, chemical type (insecticide, herbicide, fungicide, fertilizer), 
 # toxicity-human/bee, toxicity-level, state, year, and measurement(s) 
+
+pesticides <- read.csv("Pesticides.csv", header = TRUE)
+strawberries <- read.csv("Strawberries.csv", header = TRUE)
