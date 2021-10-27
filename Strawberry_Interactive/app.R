7da23142dbff078474e2a62ae35aae697c7d9607
Rose Determan <rosed@bu.edu>
2021-10-27 19:46:40 -0400

    begin shiny app

--- Strawberry_Interactive/app.R
+++ Strawberry_Interactive/app.R
@@ -7,43 +7,19 @@
 #    http://shiny.rstudio.com/
 #
 
+#YEAR and State 
+
+
+source("wrangling.R")
+
 library(shiny)
+library(shinydashboard)
 
-# Define UI for application that draws a histogram
-ui <- fluidPage(
-
-    # Application title
-    titlePanel("Group 3 Strawberry Shiny App"),
-
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput("distPlot")
-        )
-    )
+ui <- dashboardPage(
+    dashboardHeader(),
+    dashboardSidebar(),
+    dashboardBody()
 )
-
-# Define server logic required to draw a histogram
-server <- function(input, output) {
-
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
-    })
-}
-
-# Run the application 
-shinyApp(ui = ui, server = server)
+server <- function(input, output) { }
+shinyApp(ui, server)
+unun
