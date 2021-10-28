3237543b01b517f57883285395147428c86ce4c5
Rose Determan <rosed@bu.edu>
2021-10-28 08:57:19 -0400

    add barchart to shiny app

--- Strawberry_Interactive/app.R
+++ Strawberry_Interactive/app.R
@@ -1,18 +1,11 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
-#YEAR and State 
+#This is what I've put together so far. Feel free to change any of it, and we 
+#can discuss more in person too. I've mostly mirrored the example in the link 
+#below.
+#https://github.com/amrrs/sample_revenue_dashboard_shiny/blob/master/app.R
+#https://stackoverflow.com/questions/49473915/r-how-do-i-use-selectinput-in-shiny-to-change-the-x-and-fill-variables-in-a-gg
 
 
-#source: https://github.com/amrrs/sample_revenue_dashboard_shiny/blob/master/app.R
-
-source("wrangling.R")
+#source("wrangling.R")
 
 library(shiny)
 library(shinydashboard)
@@ -22,8 +15,13 @@ header <- dashboardHeader(title = "Strawberry Shiny App")
 #create sidebar
 sidebar <- dashboardSidebar(sidebarMenu(
     menuItem("Home", tabName = "dashboard", icon = icon("dashboard")),
-    menuItem("Source", icon = icon("send",lib='glyphicon'), 
-             href = "https://quickstats.nass.usda.gov/#56E84525-1350-34A1-9ED7-27363BD5A7D3")
+    menuItem("Source", icon = icon("share",lib='glyphicon'), 
+             href = "https://quickstats.nass.usda.gov/#56E84525-1350-34A1-9ED7-27363BD5A7D3"),
+    menuItem("Select X value:", 
+             selectInput('x', 'X', choices = c("State", "Year"),
+                         selected = "State")),
+    menuItem("Select Y value:",
+             selectInput('y', 'Y', choices = unique(strawberry["Measurement(s)"])))
 ))
 #create body
 frow1 <- fluidRow(
@@ -35,15 +33,15 @@ frow1 <- fluidRow(
 frow2 <- fluidRow(
     
     box(
-        title = "Revenue per Account"
+        title = "Barchart"
         ,status = "primary"
         ,solidHeader = TRUE 
         ,collapsible = TRUE 
-        ,plotOutput("revenuebyPrd", height = "300px")
+        ,plotOutput("outplot")
     )
     
     ,box(
-        title = "Revenue per Product"
+        title = "BOX2"
         ,status = "primary"
         ,solidHeader = TRUE 
         ,collapsible = TRUE 
@@ -63,7 +61,7 @@ ui <- dashboardPage(
     header,
     sidebar,
     body,
-    skin = "red"
+    skin = "green"
     
 )
 
@@ -72,16 +70,48 @@ server <- function(input, output) {
     #creating the valueBoxOutput content
     #average pesticides per acre per year
     lb_yr_ac <- strawberry[strawberry["Measurement(s)"] == " MEASURED IN LB / ACRE / YEAR",]
+    #isolate neurotoxins
+    nt <- !(lb_yr_ac$neurotoxins == '')
+    cg <- !is.na(lb_yr_ac$carcinogen)
+    
+    
     output$value1 <- renderValueBox({
         valueBox(
-            formatC(mean(lb_yr_ac$Value), format="d", big.mark=',')
-            ,paste('Mean pound per acre per year of pesticides:',mean(lb_yr_ac$Value))
-            ,icon = icon("stats",lib='glyphicon')
-            ,color = "purple")
-        
-        
-    })
+            format(mean(lb_yr_ac$Value),digits = 2)
+            ,paste('Overall pesticide application mean lb/acre/year')
+            ,color = "green")})
     
-    
-    }
+     output$value2 <- renderValueBox({
+        valueBox(
+            format(mean(lb_yr_ac$Value[nt]),digits = 2)
+            ,paste('Neurotoxin application mean lb/acre/year')
+            ,color = "blue")
+        })
+     
+     output$value3 <- renderValueBox({
+         valueBox(
+             format(round(mean(lb_yr_ac$Value[cg]), digits = 2), nsmall = 1)
+             ,paste('Carcinogen application mean lb/acre/year')
+             ,color = "yellow")
+     })
+     
+      reactive({
+      unit <- .data[[input$y]]
+      x_val <- .data[[input$x]]
+      print(typeof(x_val))
+      select <- strawberry[strawberry["Measurement(s)"] == unit,]
+      select %<>% group_by(x_val) %>% summarise_at(vars(Value),list(mean = mean))
+      
+      output$outplot <- renderPlot({
+          ggplot(select) +
+              geom_bar(aes(x = x_val, y = select$Value), position = position_stack(reverse = TRUE)) +
+              coord_flip() + 
+              theme(legend.position = "top")
+      
+     })
+      
+      })
+      
+}
 shinyApp(ui, server)
+
