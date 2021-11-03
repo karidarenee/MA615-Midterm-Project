513cb5a12573771129248394befa5114585920bc
Rose Determan <rosed@bu.edu>
2021-11-03 13:29:15 -0400

    add comments to file

--- wrangling.R
+++ wrangling.R
@@ -1,16 +1,13 @@
 # Wrangling file
 
-# GOAL: have one dataframe with the following columns: 
-# chemical, chemical type (insecticide, herbicide, fungicide, fertilizer), 
-# toxicity-human/bee, toxicity-level, state, year, and measurement(s) 
-
+#turn off warning outputs
 oldw <- getOption("warn")
 options(warn = -1)
 
 #Load packages
 pacman::p_load(tidyverse, magrittr, stringr, dplyr,janitor)
 
-# Import Data
+# Import Raw Data
 pesticides <- read.csv("Pesticides.csv", header = TRUE, fileEncoding = "latin1")
 strawb <- read.csv("Strawberries.csv", header = TRUE, fileEncoding = "latin1")
 
@@ -79,27 +76,19 @@ pesticides <- mutate_all(pesticides, .funs=toupper)
 #remove empty rows
 pesticides <- pesticides[!apply(pesticides == "", 1, all), ] 
 
-#delete white space in names
-#pesticides$name <- str_trim(pesticides$name) 
 
+#join chemical and pesticide tables and select relevant columns
 strawberry <- inner_join(strawb_chem, pesticides)
 strawberry <- select(strawberry, -c("Domain.Category", "title", "details"))
 
-# 10.27
-# break Data.item into new columns
+#break Data.item into new columns
 strawberry %<>% separate(col = Data.Item, 
                          into = c("Strawberries", "items", "discription", "units"),
                          sep = ",",
                          fill = "right")
-# explore the new columns
-distinct(strawberry, Strawberries)
-distinct(strawberry, items)
-distinct(strawberry, discription)
-distinct(strawberry, units)
-# <<<<<<< HEAD
-strawberry
+
+#drop columns with no data
 strawberry %<>%drop_na(Value)
-# =======
 
 # change names of variable "type","Carcinogen","Bee.Toxins
 colnames(strawberry)[colnames(strawberry) %in% 
@@ -115,7 +104,7 @@ colnames(strawberry)[colnames(strawberry) %in%
 #a <- sub("SLIGHT","1",strawberry$Bee.Toxins)
 #b <- sub("MODERATE","2",a)
 #c <- sub("HIGH","3",b)
-# strawberry$Bee.Toxins <- c
+
 strawberry$toxicity_bee <- plyr::mapvalues(strawberry$toxicity_bee, 
                                            from = c("SLIGHT","MODERATE","HIGH"), to = c(1,2,3))
 strawberry$toxicity_bee <- as.numeric(strawberry$toxicity_bee)
@@ -125,27 +114,18 @@ strawberry$toxicity_bee <- as.numeric(strawberry$toxicity_bee)
 strawberry$carcinogen <- plyr::mapvalues(strawberry$carcinogen, 
                                          from = c("POSSIBLE","PROBABLE","KNOWN"), to = c(1,2,3))
 strawberry$carcinogen <- as.numeric(strawberry$carcinogen)
-# >>>>>>> fc5afcadacad5040681a31f076c52d2455463e2d
-
-table(strawberry$Year)
 
 # remove missing values in carcinogen.
 strawberry_car <- subset(strawberry, carcinogen != "")
 
-table(strawberry_car$Year)
 
-table(strawberry_car$chemical)
-table(strawberry_car$chemical_type)
 # remove missing values in toxicity_bee
 strawberry_car_insec <- subset(strawberry_car, toxicity_bee != "")
-
-table(strawberry_car_insec$chemical_type)
 #' Interesting finding: after we get the strawberry_car_insec, 
 #' we found that bees are only exposed to pesticides which contains level 1 carcinogen,
 #' For pesticides containing level 2 and 3 carcinogen, there exist no bee toxins.
 
 #Subsets by different measurement
-table(strawberry$`Measurement(s)`)
 strawberry$`Measurement(s)` = str_trim(strawberry$`Measurement(s)`)
 df.lb = subset(strawberry, `Measurement(s)` == "MEASURED IN LB")
 df.app = subset(strawberry, `Measurement(s)` == "MEASURED IN LB / ACRE / APPLICATION")
@@ -154,22 +134,21 @@ df.num = subset(strawberry, `Measurement(s)` == "MEASURED IN NUMBER")
 df.PCT = subset(strawberry, `Measurement(s)` == "MEASURED IN PCT OF AREA BEARING")
 
 #Calculate proportions of strawberries by 'Measurements=Number'
-library(dplyr)
 df.num<-tibble::as_tibble(df.num)
+
 #Count frequency/proportion for each state
 df.num %>%
   group_by(State) %>%
   summarise(n = n()) %>%
   mutate(freq = n / sum(n))
-#I think we can show the proportions of each states on our map using mouse over.
 
 #Calculate proportions of values(numbers) grouped by 'toxicity_bee'.
 df.value<-tibble::as_tibble(df.num$Value)
 df.toxicity_bee<-tibble::as_tibble(df.num$toxicity_bee)
 df.toxicity<-cbind(df.value,df.toxicity_bee)
+
 #Rename column 2 into 'toxicity_bee'
 names(df.toxicity)[2]<- 'toxicity_bee'
-
 df.toxicity %>%
   group_by(toxicity_bee) %>%
   summarise(n = n()) %>%
